b6964a1823d5c9142553f2ba5dd9e3ea48410682
not1cyyy <firas1120@gmail.com>
2024-08-21 22:48:46 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# egg-fried-rice
-Don't act like you don't know what this is 
+# Dotfiles
+ 
+ 
+# Preview:
+
+![image](https://github.com/ART3MISTICAL/dotfiles/assets/68769374/7b2a06b2-67d6-4ced-b558-a6fdc142cd05)
+
---
+++ config/fish/config.fish
@@ -0,0 +1,16 @@
+set fish_greeting
+
+starship init fish | source
+
+#alias code="blur 'code .' 'Code' 5"
+
+alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
+alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first --icons'  # long format
+alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
+alias l.="exa -a | egrep '^\.'"                                     # show only dotfiles
+alias ip="ip -color"
+
+if status --is-interactive
+   neofetch
+end
